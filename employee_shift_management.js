bc08ce0840dd731097d66bbf9512a302369cae5c
Roan Ali <alir13@usf.edu>
2024-09-22 10:59:01 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,16 @@
+//Task 1: Creating an Employees Array of Employee Objects
+
+//Array for employee information
+const employees = [
+
+ { name: 'Alicia Ali', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Wednesday', hours: 6 }] },
+    
+ { name: 'Sarah Lee', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 7 }] },
+    
+ { name: 'Bob Millers', shifts: [{ day: 'Monday', hours: 8 }] },
+    
+ { name: 'Apple Jane', shifts: [{ day: 'Friday', hours: 8 }] }
+];
+
+console.log("Employee Shift Details:",(employees));
+    
