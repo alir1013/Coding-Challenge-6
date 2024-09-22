3b8171f2c7ba1bfef9dbb6da0f75ba988591bd3e
Roan Ali <alir13@usf.edu>
2024-09-22 12:12:48 -0400

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -4,13 +4,26 @@
 const employees = [
 
  { name: 'Alicia Ali', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Wednesday', hours: 6 }] },
-    
  { name: 'Sarah Lee', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 7 }] },
-    
  { name: 'Bob Millers', shifts: [{ day: 'Monday', hours: 8 }] },
-    
  { name: 'Apple Jane', shifts: [{ day: 'Friday', hours: 8 }] }
 ];
 
 console.log("Employee Shift Details:",(employees));
     
+//Task 2: Creating a Function to Display Employee Shift Details
+console.log("Employee Shift Details:"); //For better Readibility
+
+//Creating a function that accepts an employee object and logs employee shift info
+function displayEmployeeShifts(employees,employeeName){
+    employees.forEach(employee => {
+        if(employee.name === employeeName){
+            console.log(`${employee.name}`);
+            employee.shifts.forEach(shift => {
+                console.log(`Assigned Shifts: ${shift.day}, Hours Worked: ${shift.hours}`); //Logs the employee’s name and their assigned shifts
+            });
+        }
+    });
+}
+    
+displayEmployeeShifts(employees, `Bob Millers`); //Testing the Function
